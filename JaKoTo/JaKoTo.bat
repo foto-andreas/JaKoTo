938e3eec09b8c706aeeffa9b1adea96bc320645b
Andreas Schrell <as@schrell.de>
2010-02-20 17:16:03 +0100

    Type bei Library-Path: ";" statt ":" funktioniert besser.

--- JaKoTo/JaKoTo.bat
+++ JaKoTo/JaKoTo.bat
@@ -1,1 +1,1 @@
-java -jar -splash:lib\armotool.png -Xmx256m -Djava.library.path=lib\rxtx32:lib\rxtx64 "JaKoTo.jar"
+java -jar -splash:lib\armotool.png -Xmx256m -Djava.library.path=lib\rxtx32;lib\rxtx64 "JaKoTo.jar"
